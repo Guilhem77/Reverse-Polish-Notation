460e61477584dc74dd51237a91af163daf06b5c6
Vash94350 <michael.pean.mp@gmail.com>
2018-05-23 16:31:06 +0200

    Ajout d'une class Evaluator qui rend notre code plus lisible et plus adaptable (creation de sous methode a partir de notre ancienne evaluate())

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -1,6 +1,5 @@
 package rpn;
 
-import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,46 +8,10 @@ public class CLI {
         String expression = Stream.of(args).collect(Collectors.joining(" "));
 
         System.out.println("About to evaluate '" + expression + "'");
-        long result = evaluate(expression);
+
+        Evaluator evaluator = new Evaluator();
+        long result = evaluator.evaluate(expression);
+
         System.out.println("> " + result);
     }
-
-    static long evaluate(String expression) throws DivideException {
-        long result = 0;
-        int value = 0;
-        Stack<Integer> numbers = new Stack<>();
-        String [] symbols = expression.split(" ");
-
-        for (String str : symbols) {
-            if (!(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"))) {
-                int number = Integer.parseInt(str);
-                numbers.push(number);
-            }
-            else {
-                int number1 = numbers.pop();
-                int number2 = numbers.pop();
-                if (str.equals("+")) {
-                    value = number2 + number1;
-                }
-                else if (str.equals("-")) {
-                    value = number2 - number1;
-                }
-                else if (str.equals("*")) {
-                    value = number2 * number1;
-                }
-                else if (str.equals("/")) {
-                    if (number1 != 0) {
-                        value = number2 / number1;
-                    }
-                    else {
-                        throw new DivideException("Division par 0 impossible.");
-                    }
-                }
-                numbers.push(value);
-            }
-        }
-        result = numbers.pop();
-        return result;
-    }
-
 }
